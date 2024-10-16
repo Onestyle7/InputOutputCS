7ae0bedd2226f03f31aa062da9754c035c9cf1a4
Onestyle7 <klusbartek8@gmail.com>
2024-10-16 13:47:40 +0200

    Added connection to Json File and two methotds
    - Add Read file method which read all json file by Newton.json and deserialize it to list of contacts
    
    - Add Write file method which Serialize passed object to List

--- InputOutput/Data/DataAccess.cs
+++ InputOutput/Data/DataAccess.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using InputOutput.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace InputOutput.Data
 {
     public class DataAccess
     {
+        private string _filePath;
+
+        public DataAccess(string filePath) 
+        {
+            _filePath = filePath;
+        }
+        public List<Contact> ReadUserDefinedContacts()
+        {
+            using StreamReader reader = new StreamReader(_filePath);
+            var json = reader.ReadToEnd();
+
+            List<Contact> contacts = JsonConvert.DeserializeObject<List<Contact>>(json);
+
+            return contacts;
+        }
+        public void WriteUserDefinedContacts(List<Contact> contacts)
+        {
+            var json = JsonConvert.SerializeObject(contacts, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
     }
 }
