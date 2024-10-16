f38ef24754aa7fc5ac143fff32688f723565c1ea
Onestyle7 <klusbartek8@gmail.com>
2024-10-16 13:51:24 +0200

    Tested methods for reading json file and adding content.
    Currently user cannot add Contact to the Contacts.json from the console

--- InputOutput/Program.cs
+++ InputOutput/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace InputOutput
+﻿using InputOutput.Data;
+using InputOutput.Models;
+
+namespace InputOutput
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = "C:\\Users\\klusb\\Desktop\\InputOutputCS\\InputOutput\\Data\\contact.json";
+            DataAccess dataAccess = new DataAccess(filePath);
+            List<Contact> contacts = dataAccess.ReadUserDefinedContacts();
+
+            foreach(var contact in contacts)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine("-----------------------------");
+            }
+
+            Contact newContact = new Contact
+            {
+                FirstName = "Ted",
+                LastName = "Doom",
+                PhoneNumber = "723-455-7890",
+                Email = "Ted@gmail.com"
+            };
+
+            contacts.Add(newContact);
+            dataAccess.WriteUserDefinedContacts(contacts);
+            Console.WriteLine("Dodaje kontakty....");
+            Thread.Sleep(2000);
+
+            var lastContact = contacts.Last();
+            Console.WriteLine("Ostatni kontakt dodany:");
+            Console.WriteLine($"First Name: {lastContact.FirstName}");
+            Console.WriteLine($"Last Name: {lastContact.LastName}");
+            Console.WriteLine($"Phone Number: {lastContact.PhoneNumber}");
+            Console.WriteLine($"Email: {lastContact.Email}");
+
+
+            Console.ReadLine();
         }
     }
 }
